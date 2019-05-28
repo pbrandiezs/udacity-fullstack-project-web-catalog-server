064a0e17e921c7038387262d09ca1e63453bfc1b
Perry Brandiezs <pbrandiezs@gmail.com>
2019-05-28 15:47:56 -0700

    feat: added create_planes.py and models.py

---
+++ create_planes.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python3
+
+# Program: create_planes.py
+# Author: Perry Brandiezs
+# Date: May 1, 2019
+# Last Updated: May 28, 2019
+#
+# This program populates the ItemCatalog database
+# with users and several planes for testing.
+#
+
+from sqlalchemy import create_engine
+from sqlalchemy import exc
+from sqlalchemy.orm import sessionmaker
+from models import Base, User, ItemCatalog, Category
+import logging
+
+# Set logger level to info
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Connect to Database and create database session
+engine = create_engine('sqlite:///ItemCatalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# Add users
+NewUser = User(
+    email="pbrandiezs@gmail.com",
+    id=1,
+    picture="https://platform-lookaside.fbsbx.com/platform/profilepic/"
+            "?asid=2296167867307191"
+            "&height=200&width=200"
+            "&ext=1559163571"
+            "&hash=AeTcZcYDm6Y6E-zk",
+    username="Perry Brandiezs"
+    )
+try:
+    session.add(NewUser)
+    session.commit()
+    logging.info("User %s added" % NewUser.username)
+except exc.IntegrityError:
+    logging.warning("Not added - User %s" % NewUser.username)
+    session.rollback()
+
+NewUser = User(
+    email="mickeymouse@disney.com",
+    id=2,
+    picture="https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/"
+            "Mickey_Mouse.png/220px-Mickey_Mouse.png",
+    username="Mickey Mouse"
+    )
+try:
+    session.add(NewUser)
+    session.commit()
+    logging.info("User %s added" % NewUser.username)
+except exc.IntegrityError:
+    logging.warning("Not added - User %s" % NewUser.username)
+    session.rollback()
+
+# Add Categories
+NewCategory = Category(
+    id=1,
+    category_name="Gulfstream"
+    )
+try:
+    session.add(NewCategory)
+    session.commit()
+    logging.info("Category %s added" % NewCategory.category_name)
+except exc.IntegrityError:
+    logging.warning("Not added - Category %s" % NewCategory.category_name)
+    session.rollback()
+
+NewCategory = Category(
+    id=2,
+    category_name="Crop Duster"
+    )
+try:
+    session.add(NewCategory)
+    session.commit()
+    logging.info("Category %s added" % NewCategory.category_name)
+except exc.IntegrityError:
+    logging.warning("Not added - Category %s" % NewCategory.category_name)
+    session.rollback()
+
+NewCategory = Category(
+    id=3,
+    category_name="Falcon"
+    )
+try:
+    session.add(NewCategory)
+    session.commit()
+    logging.info("Category %s added" % NewCategory.category_name)
+except exc.IntegrityError:
+    logging.warning("Not added - Category %s" % NewCategory.category_name)
+    session.rollback()
+
+NewCategory = Category(
+    id=4,
+    category_name="Embraer"
+    )
+try:
+    session.add(NewCategory)
+    session.commit()
+    logging.info("Category %s added" % NewCategory.category_name)
+except exc.IntegrityError:
+    logging.warning("Not added - Category %s" % NewCategory.category_name)
+    session.rollback()
+
+NewCategory = Category(
+    id=5,
+    category_name="Aerospatiale/BAC"
+    )
+try:
+    session.add(NewCategory)
+    session.commit()
+    logging.info("Category %s added" % NewCategory.category_name)
+except exc.IntegrityError:
+    logging.warning("Not added - Category %s" % NewCategory.category_name)
+    session.rollback()
+
+# Add planes
+NewPlane = ItemCatalog(
+    id=1,
+    category_id=1,
+    item_name="G650ER",
+    item_description="Long range private jet",
+    user_id=1
+    )
+try:
+    session.add(NewPlane)
+    session.commit()
+    logging.info("Plane %s added" % NewPlane.item_name)
+except exc.IntegrityError:
+    logging.warning("Not added - Plane %s" % NewPlane.item_name)
+    session.rollback()
+
+NewPlane = ItemCatalog(
+    id=2,
+    category_id=1,
+    item_name="G550",
+    item_description="A very nice jet",
+    user_id=1
+    )
+try:
+    session.add(NewPlane)
+    session.commit()
+    logging.info("Plane %s added" % NewPlane.item_name)
+except exc.IntegrityError:
+    logging.warning("Not added - Plane %s" % NewPlane.item_name)
+    session.rollback()
+
+NewPlane = ItemCatalog(
+    id=3,
+    category_id=1,
+    item_name="G650ER",
+    item_description="Mickey Mouse's Long range private jet",
+    user_id=2
+    )
+try:
+    session.add(NewPlane)
+    session.commit()
+    logging.info("Plane %s added" % NewPlane.item_name)
+except exc.IntegrityError:
+    logging.warning("Not added - Plane %s" % NewPlane.item_name)
+    session.rollback()
+
+NewPlane = ItemCatalog(
+    id=4,
+    category_id=2,
+    item_name="Piper Cub",
+    item_description="Little yellow airplane that spews toxic chemicals"
+                     " on the food supply to kill beneficial insects.",
+    user_id=1
+    )
+try:
+    session.add(NewPlane)
+    session.commit()
+    logging.info("Plane %s added" % NewPlane.item_name)
+except exc.IntegrityError:
+    logging.warning("Not added - Plane %s" % NewPlane.item_name)
+    session.rollback()
+
+NewPlane = ItemCatalog(
+    id=5,
+    category_id=3,
+    item_name="10X",
+    item_description="The Dassault Mystere/Falcon 10 is an early corporate"
+                     " jet aircraft developed by French aircraft"
+                     " manufacturer Dassault Aviation.",
+    user_id=1
+    )
+try:
+    session.add(NewPlane)
+    session.commit()
+    logging.info("Plane %s added" % NewPlane.item_name)
+except exc.IntegrityError:
+    logging.warning("Not added - Plane %s" % NewPlane.item_name)
+    session.rollback()
+
+NewPlane = ItemCatalog(
+    id=6,
+    category_id=4,
+    item_name="Phenom 100",
+    item_description="The Embraer EMB-500 Phenom 100 is a very light jet"
+                     " developed by Brazilian aircraft manufacturer Embraer.",
+    user_id=1
+    )
+try:
+    session.add(NewPlane)
+    session.commit()
+    logging.info("Plane %s added" % NewPlane.item_name)
+except exc.IntegrityError:
+    logging.warning("Not added - Plane %s" % NewPlane.item_name)
+    session.rollback()
+
+NewPlane = ItemCatalog(
+    id=7,
+    category_id=5,
+    item_name="Concorde",
+    item_description="The Aerospatiale/BAC Concorde is a French-British"
+                     " turbojet-powered supersonic passenger airliner that"
+                     " was operated from 1976 until 2003. It had a maximum"
+                     " speed over twice the speed of sound at Mach 2.04"
+                     " (1,354 mph or 2,180 km/h at cruise altitude)"
+                     ", with seating for 92 to 128 passengers.",
+    user_id=1
+    )
+try:
+    session.add(NewPlane)
+    session.commit()
+    logging.info("Plane %s added" % NewPlane.item_name)
+except exc.IntegrityError:
+    logging.warning("Not added - Plane %s" % NewPlane.item_name)
+    session.rollback()
---
+++ models.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+# Program: models.py
+# Author: Perry Brandiezs
+# Date: May 1, 2019
+# Last Updated: May 28, 2019
+
+# This program sets up the database used by application.py
+
+
+from sqlalchemy import Column, ForeignKey, Integer, String, UniqueConstraint
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker, backref
+from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+import random
+import string
+from itsdangerous import (
+    TimedJSONWebSignatureSerializer as Serializer,
+    BadSignature,
+    SignatureExpired)
+
+Base = declarative_base()
+
+# You will use this secret key to create and verify your tokens
+secret_key = ''.join(
+    random.choice(
+        string.ascii_uppercase + string.digits) for x in xrange(32))
+
+
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, index=True)
+    password_hash = Column(String)
+    email = Column(String)
+    picture = Column(String)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.hash(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(secret_key)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            # Valid Token, but expired
+            return None
+        except BadSignature:
+            # Invalid Token
+            return None
+        user_id = data['id']
+        return user_id
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format. """
+        return {
+            'id': self.id,
+            'username': self.username,
+            'password_hash': self.password_hash,
+            'email': self.email,
+            'picture': self.picture
+            }
+
+
+class Category(Base):
+    __tablename__ = 'Category'
+    id = Column(Integer, primary_key=True)
+    category_name = Column(String, index=True, unique=True)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format. """
+        return {
+            'id': self.id,
+            'category_name': self.category_name
+            }
+
+
+class ItemCatalog(Base):
+    __tablename__ = 'ItemCatalog'
+    id = Column(Integer, primary_key=True)
+    category_id = Column(Integer, ForeignKey('Category.id'))
+    item_name = Column(String)
+    item_description = Column(String)
+    user_id = Column(Integer, ForeignKey('User.id'))
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format."""
+        return {
+            'id': self.id,
+            'category_id': self.category_id,
+            'item_name': self.item_name,
+            'item_description': self.item_description,
+            'user_id': self.user_id
+            }
+
+
+engine = create_engine('sqlite:///ItemCatalog.db')
+Base.metadata.create_all(engine)
